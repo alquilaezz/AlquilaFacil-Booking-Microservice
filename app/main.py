4db68de24a30cd66907fc17e52c0ce53dfc8efcf
JCarbajal22 <u202121881@upc.edu.pe>
2025-12-05 01:26:57 -0500

    Revert de cambio inutil

--- app/main.py
+++ app/main.py
@@ -1,13 +1,10 @@
 from fastapi import FastAPI
 from .database import Base, engine
-from . import models
 from .routers import reservations, reports
 
-app = FastAPI(title="Booking Service")
+Base.metadata.create_all(bind=engine)
 
-@app.on_event("startup")
-def on_startup():
-    Base.metadata.create_all(bind=engine)
+app = FastAPI(title="Booking Service")
 
 app.include_router(reservations.router)
 app.include_router(reports.router)
