3104c0631bfd1d385da1064abcc38feed417bb3d
JCarbajal22 <u202121881@upc.edu.pe>
2025-12-05 01:21:10 -0500

    Cambios

--- app/main.py
+++ app/main.py
@@ -1,10 +1,13 @@
 from fastapi import FastAPI
 from .database import Base, engine
+from . import models
 from .routers import reservations, reports
 
-Base.metadata.create_all(bind=engine)
-
 app = FastAPI(title="Booking Service")
+
+@app.on_event("startup")
+def on_startup():
+    Base.metadata.create_all(bind=engine)
 
 app.include_router(reservations.router)
 app.include_router(reports.router)
